2217b0b4864b398470a19d6b53fec720faaeebba
nikhilkumar50 <nikhilkumarray9889@gmail.com>
2023-07-29 01:46:31 +0530

    redux concept 1

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log('hello');
+import {createStore,applyMiddleware} from 'redux';
+import logger from 'redux-logger';
+// create a store
+const store=createStore(reducer,applyMiddleware(logger.default));
+
+ //reducer is a function
+function reducer(state={amount:100},action){
+    if(action.type==='increment'){
+        //immutability is required
+        return {amount:state.amount+action.payload};
+
+    }
+    
+    return state;
+}
+const history=[];
+
+//global state
+// store.subscribe(()=>{
+//     console.log(history.push(store.getState()));
+//     console.log(history);
+// })
+
+//action
+setInterval(()=>{
+    store.dispatch({type:'increment',payload:100});
+
+},5000)
+
+
+
+
